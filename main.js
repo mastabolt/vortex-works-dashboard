9bb38309538dd5563cafa53146f6208b5d0e3087
mastabolt <mastabolt@gmail.com>
2025-08-07 22:14:02 -0400

    Updated to New version

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
-import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <h1>Zoho Dashboard Connected!</h1>
-  <p>This is a test dashboard using Vite.</p>
-`
+import { BuilderComponent, builder } from '@builder.io/react';
+console.log('Builder connected');
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: './'
-})
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+export default defineConfig({ plugins: [react()] });
